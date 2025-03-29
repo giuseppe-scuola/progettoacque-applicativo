2d8dcc3c92f06067ae8babc47e538841a22854c5
Giuseppe di Pasquale <giuseppedipasquale@iisdaostalaquila.it>
2025-03-29 11:31:23 +0000

    Aggiunto il file principale

---
+++ main.cpp
@@ -0,0 +1,223 @@
+/*
+    Appunti:
+    stoul per convertire interi
+    stod per convertire decimali
+*/
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+#if defined(_WIN32) || defined(_WIN64)
+#define PulisciSchermo system("cls")
+#else
+#define PulisciSchermo system("clear")
+#endif
+
+#define LunghezzaDatabase 16
+#define SpeciePerRecord 8
+
+
+struct batterio
+{
+    std::string NomeBatterio;
+    double Concentrazione;
+};
+
+struct record
+{
+    int Luogo;
+    std::string Longitudine;
+    std::string Latitudine;
+    unsigned int NumeroSpecie;
+    batterio Dati[SpeciePerRecord];
+};
+
+
+void ApriFile(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto, std::ifstream &FileInput);
+
+int Menu();
+bool InputInt(unsigned int &Valore);
+
+void Inserimento(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto);
+bool InputDouble(double &Valore);
+
+void Stampa(record Array[LunghezzaDatabase], unsigned int IndiceVuoto);
+
+void Modifica(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto);
+
+void CancellaPrompt(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto);
+void Cancella(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto, unsigned int DaRimuovere);
+
+void Ricerca(record Array[LunghezzaDatabase], unsigned int IndiceVuoto);
+
+void Grafico(record Array[LunghezzaDatabase], unsigned int IndiceVuoto);
+
+void SalvaSuFilePrompt(record Array[LunghezzaDatabase], unsigned int IndiceVuoto);
+void SalvaSuFile(record Array[LunghezzaDatabase], unsigned int IndiceVuoto, std::ofstream &FileOutput);
+
+
+int main()
+{
+    //Dichiarazione variabili
+    bool Modificato = false, Termina = false;
+    unsigned int IndiceDatabase = 0, Contatore = 0;
+    int Scelta = -1;
+    record Database[LunghezzaDatabase];
+
+    //Inizializzazione programma
+    setlocale(LC_ALL, "italian");
+    std::cout << "Benvenuto nell'applicativo per il progetto delle acque!\nNumero massimo di record: " << LunghezzaDatabase << "\nNumero di specie per record: " << SpeciePerRecord << std::endl;
+    std::ifstream FileDatabase("Batteri.csv");
+    ApriFile(Database, IndiceDatabase, FileDatabase);
+    FileDatabase.close();
+
+    //Loop infinito
+    while ((Contatore < LunghezzaDatabase) && (Termina != true))
+    {
+        //Menù
+        Scelta = Menu();
+
+        //Pulizia schermo
+        PulisciSchermo;
+
+        //Gestione input dell'utente
+        switch (Scelta)
+        {
+            case 0:
+                Termina = true;
+                SalvaSuFilePrompt(Database, IndiceDatabase);
+                break;
+            case 1:
+                PulisciSchermo;
+                Inserimento(Database, IndiceDatabase);
+                break;
+            case 2:
+                PulisciSchermo;
+                Stampa(Database, IndiceDatabase);
+                break;
+            case 3:
+                PulisciSchermo;
+                Modifica(Database, IndiceDatabase);
+                break;
+            case 4:
+                PulisciSchermo;
+                CancellaPrompt(Database, IndiceDatabase);
+                break;
+            case 5:
+                PulisciSchermo;
+                Ricerca(Database, IndiceDatabase);
+                break;
+            case 6:
+                std::cout << "Funzione non ancora disponibile..." << std::endl;
+                break;
+            default:
+                std::cout << "Questa opzione non esiste!" << std::endl;
+                break;
+        }
+    };
+
+    //Fine programma
+    return 0;
+}
+
+void ApriFile(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto, std::ifstream &FileInput)
+{}
+
+int Menu() //divertiti federi
+{}
+
+bool InputInt(unsigned int &Valore)
+{}
+
+void Inserimento(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto)
+{}
+
+bool InputDouble(double &Valore)
+{}
+
+void Stampa(record Array[LunghezzaDatabase], unsigned int IndiceVuoto)
+{}
+
+void Modifica(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto)
+{}
+
+void CancellaPrompt(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto)
+{}
+
+void Cancella(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto, unsigned int DaRimuovere)
+{}
+
+void Ricerca(record Array[LunghezzaDatabase], unsigned int IndiceVuoto)
+{}
+
+void Grafico(record Array[LunghezzaDatabase], unsigned int IndiceVuoto)
+{}
+
+void SalvaSuFilePrompt(record Array[LunghezzaDatabase], unsigned int IndiceVuoto)
+{
+    //Dichiarazione variabili
+    bool FileInvalido = true;
+    std::string NomeFile;
+    std::ofstream FileOutput;
+
+    //Input con verifica
+    while (FileInvalido)
+    {
+        //Output per l'utente
+        std::cout << "Inserisci il nome del file (* per sovrascivere database, ? per annullare salvataggio): ";
+        std::getline(std::cin, NomeFile);
+
+        //Controllo per caratteri speciali o non permessi
+        if ((NomeFile.find('\\') != std::string::npos) || (NomeFile.find('%') != std::string::npos) || (NomeFile.find(':') != std::string::npos) || (NomeFile.find('|') != std::string::npos) || (NomeFile.find('"') != std::string::npos) || (NomeFile.find('<') != std::string::npos) || (NomeFile.find('>') != std::string::npos))
+        {
+            std::cout << "Il nome del file non è valido!" << std::endl;
+        }
+        else if (NomeFile.find('?') != std::string::npos)
+        {
+            FileInvalido = false;
+        }
+        else if (NomeFile.find('*') != std::string::npos)
+        {
+            FileInvalido = false;
+            FileOutput.open("Database.csv");
+            SalvaSuFile(Array, IndiceVuoto, FileOutput);
+            FileOutput.close();
+        }
+        else
+        {
+            FileInvalido = false;
+            FileOutput.open(NomeFile);
+            SalvaSuFile(Array, IndiceVuoto, FileOutput);
+            FileOutput.close();
+        }
+    }
+
+    //Pulizia schermo e prompt di fine programma
+    PulisciSchermo;
+    std::cout << "Il file è stato salvato." << std::endl;
+}
+
+void SalvaSuFile(record Array[LunghezzaDatabase], unsigned int IndiceVuoto, std::ofstream &FileOutput)
+{
+    //Dichiarazione variabili
+    unsigned int Contatore = 0, ContatoreSpecie = 0;
+
+    //Scrittura su file
+    while (Contatore < IndiceVuoto)
+    {
+        //Sezione inziale del file
+        FileOutput << Array[Contatore].Luogo << ';' << Array[Contatore].Longitudine << ';' << Array[Contatore].Latitudine << ';' << Array[Contatore].NumeroSpecie << ';';
+
+        //Scrittura batteri
+        while (ContatoreSpecie < Array[Contatore].NumeroSpecie)
+        {
+            FileOutput << Array[Contatore].Dati[Contatore].NomeBatterio << ';' << Array[Contatore].Dati[Contatore].Concentrazione << ';';
+            ContatoreSpecie++;
+        };
+        ContatoreSpecie = 0;
+        Contatore++;
+
+        //Fine riga
+        FileOutput << std::endl;
+    }
+}
