d044fa78d8cf9127558d0c3a38250b6c3b10365d
Giuseppe di Pasquale <giuseppedipasquale@iisdaostalaquila.it>
2025-04-04 10:50:28 +0000

    Inizio della funzione di cancellazione

--- Funzioni/Cancella.hpp
+++ Funzioni/Cancella.hpp
@@ -1,5 +1,18 @@
 void CancellaPrompt(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto)
-{}
+{
+    //Dichiarazione variabili
+    bool InputValido = false;
+
+    //Loop per l'input
+    while (!InputValido)
+    {
+        //Output per l'utente
+        Stampa(Array, IndiceVuoto);
+    }
+}
 
 void Cancella(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto, unsigned int DaRimuovere)
-{}
+{
+    //Loop per la rimozione
+    while ()
+}
