83970bfd3304ee065d3640902f8953c6cb86df09
Giuseppe di Pasquale <giuseppedipasquale@iisdaostalaquila.it>
2025-04-04 10:00:01 +0000

    Update Menu.hpp

---
+++ Funzioni/Menu.hpp
@@ -0,0 +1,14 @@
+int Menu()
+{
+    int Choose;
+    std::cout<<"inserisci:"<<std::endl;
+    std::cout<<"1- per fare l'inserimento"<<std::endl;
+    std::cout<<"2- per stampare"<<std::endl;
+    std::cout<<"3- per fare una modifica"<<std::endl;
+    std::cout<<"4- per cancellare"<<std::endl;
+    std::cout<<"5- per fare la ricerca"<<std::endl;
+    std::cout<<"6- per realizzare il grafico"<<std::endl;
+    std::cout<<"0- per uscire e salvare sul file"<<std::endl;
+    std::cin>>Choose;
+    return(Choose);
+}
