36f8366e27f6534013c33dfbb9a87d3ebdc1056f
Giuseppe di Pasquale <giuseppedipasquale@iisdaostalaquila.it>
2025-04-04 09:58:03 +0000

    Update Grafico.hpp

---
+++ Funzioni/Grafico.hpp
@@ -0,0 +1,8 @@
+void Grafico(record Array[LunghezzaDatabase], unsigned int IndiceVuoto)
+{
+    #if defined(_WIN32) || defined(_WIN64)
+    //Codice qui
+    #else
+    std::cout << "Funzione non supportata su questo sistema operativo..." << std::endl;
+    #endif
+}
