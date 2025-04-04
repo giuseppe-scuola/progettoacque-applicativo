181e03571d5e0346c38db32ec0badf62cf58e293
Giuseppe di Pasquale <giuseppedipasquale@iisdaostalaquila.it>
2025-04-04 10:18:05 +0000

    Update Menu.hpp

--- Funzioni/Menu.hpp
+++ Funzioni/Menu.hpp
@@ -1,14 +1,32 @@
 int Menu()
 {
-    int Choose;
-    std::cout<<"inserisci:"<<std::endl;
-    std::cout<<"1- per fare l'inserimento"<<std::endl;
-    std::cout<<"2- per stampare"<<std::endl;
-    std::cout<<"3- per fare una modifica"<<std::endl;
-    std::cout<<"4- per cancellare"<<std::endl;
-    std::cout<<"5- per fare la ricerca"<<std::endl;
-    std::cout<<"6- per realizzare il grafico"<<std::endl;
-    std::cout<<"0- per uscire e salvare sul file"<<std::endl;
-    std::cin>>Choose;
-    return(Choose);
+    //Dichiarazione variabili
+    bool InputValido = false;
+    unsigned int Scelta;
+
+    while (!InputValido)
+    {
+        //Output per l'utente
+        std::cout << "Scegli uno tra i seguenti numeri;" << std::endl;
+        std::cout << "[0] Salva ed esci..." << std::endl; 
+        std::cout << "[1] per fare l'inserimento" << std::endl;
+        std::cout << "[2] per stampare" << std::endl;
+        std::cout << "[3] per fare una modifica" << std::endl;
+        std::cout << "[4] per cancellare" << std::endl;
+        std::cout << "[5] Cerca un record..." << std::endl;
+        std::cout << "[6] Genera un grafico..." << std::endl;
+        std::cout << "Inserisci un numero: ";
+        InputValido = InputInt(Scelta);
+
+        //Gestione dell'input
+        if (!InputDouble)
+        {
+            //Pulizia schermo e output per l'utente
+            PulisciSchermo;
+            std::cout << "Il valore inserito non è valido." << std::endl;
+        }
+    }
+
+    //Restituzione del valore
+    return Scelta;
 }
