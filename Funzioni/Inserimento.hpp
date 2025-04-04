aef3c16a3e45a8598b751692ef346051b80fd50a
FedericaPacitti <federicapacitti@iisdaostalaquila.it>
2025-04-04 12:26:33 +0200

    Update Inserimento.hpp

--- Funzioni/Inserimento.hpp
+++ Funzioni/Inserimento.hpp
@@ -7,49 +7,50 @@ void Inserimento(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto)
     std::cout<<"1)Singolo"<<std::endl;
     std::cout<<"2)Massivo"<<std::endl;
     std::cin>>Scelta;
+    bool ris=false;
 
     switch(Scelta){
 
         case 1: PulisciSchermo;
                 std::cout<<"Hai scelto l'inserimento singolo"<<std::endl;
                 std::cout<<std::endl;
-
                 std::cout<<"Qual è il nome del luogo?"<<std::endl;
                 NomeLuogo(Array, IndiceVuoto);
-
-                PulisciSchermo;
-                std::cout<<"Inserisci il numero della specie: ";
-                std::cin>>Array[IndiceVuoto].NumeroSpecie;
                 std::cout<<std::endl;
-
-                for(int k=0, k<SpeciePerRecord, k++){
-                    std::cout<<"Inserisci il nome del "<<k+1<<"° batterio: "<<std::endl;
-                    std::cin>>Array[IndiceVuoto].NomeBatterio;
-                    std::cout<<"Inserisci la concentrazione"<<std::endl;
-                    std::cin>>Array[IndiceVuoto].Concentrazione;
+                std::cout<<"Inserisci il nome del batterio: "<<std::endl;
+                std::cin>>Array[IndiceVuoto].Dati[Array[IndiceVuoto].NumeroSpecie].NomeBatterio;
+                for(int k=0; k<SpeciePerRecord; k++){
+                    while(ris==false){
+                        std::cout<<"Inserisci la concentrazione"<<std::endl;
+                        std::cin>>Array[IndiceVuoto].Dati[k].Concentrazione;
+                        ris=InputDouble(Array[IndiceVuoto].Dati[k].Concentrazione)
+                        if(ris==false){
+                            std::cout<<"Inserimento non valido."<<std::endl;
+                        }
+                    }
                 }
-
         break;
         case 2: PulisciSchermo;
-                int Num_Massivi;
+                int NumMassivi;
                 std::cout<<"Hai scelto l'inserimento massivo"<<std::endl;
                 std::cout<<std::endl;
 
                 std::cout<<"Quanti dati vuoi inserire? ";
-                std::cin>>Num_Massivi;
-                for(int i=0; i<Num_Massivi; i++)
+                std::cin>>NumMassivi;
+                for(int i=0; i<NumMassivi; i++)
                 {
                     std::cout<<"Qual è il nome del "<<i+1"° luogo?"<<std::endl;
                     NomeLuogo(Array, IndiceVuoto);
-                    PulisciSchermo;
-                    std::cout<<"Inserisci il numero della specie: ";
-                    InputDouble(Array[IndiceVuoto].NumeroSpecie);
-                    std::cout<<std::endl;
                     for(int j=0; j<SpeciePerRecord; j++){
                         std::cout<<"Inserisci il nome del "<<j+1<<"° batterio: "<<std::endl;
                         std::cin>>Array[IndiceVuoto].Dati[Array[IndiceVuoto].NumeroSpecie].NomeBatterio;
                         std::cout<<"Inserisci la concentrazione"<<std::endl;
                         std::cin>>Array[IndiceVuoto].Dati[Array[IndiceVuoto].NumeroSpecie].Concentrazione;
+                        ris=InputDouble(Array[IndiceVuoto].Dati[Array[IndiceVuoto].NumeroSpecie].Concentrazione)
+                        if(ris==false){
+                            std::cout<<"Inserimento non valido."<<std::endl;
+                        }
+
                     }
                 }
         break;
