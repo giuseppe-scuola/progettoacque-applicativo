98464df30043f0b87186acec72874d784c056b49
Giuseppe di Pasquale <giuseppedipasquale@iisdaostalaquila.it>
2025-04-04 09:57:47 +0000

    Update Cancella.hpp

---
+++ Funzioni/Cancella.hpp
@@ -0,0 +1,5 @@
+void CancellaPrompt(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto)
+{}
+
+void Cancella(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto, unsigned int DaRimuovere)
+{}
