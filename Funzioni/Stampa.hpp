d7a8e07ef645c3e4028c11f395b4f2cbd6885e3d
Giuseppe di Pasquale <giuseppedipasquale@iisdaostalaquila.it>
2025-04-04 12:53:27 +0000

    Rimozione std::

--- Funzioni/Stampa.hpp
+++ Funzioni/Stampa.hpp
@@ -3,13 +3,13 @@ void Stampa(record Array[LunghezzaDatabase], unsigned int IndiceVuoto)
     unsigned int conta=0;
     while(conta<=IndiceVuoto)
     {
-        std::cout<<Array[IndiceVuoto].Latitudine<<"|"<<Array[IndiceVuoto].Longitudine<<"|"<<Array[IndiceVuoto].NumeroSpecie<<'\t';
+        cout<<Array[IndiceVuoto].Latitudine<<"|"<<Array[IndiceVuoto].Longitudine<<"|"<<Array[IndiceVuoto].NumeroSpecie<<'\t';
         for(int k=0; k<8; k++)
         {
-            std::cout<<Array[IndiceVuoto].Dati[k].NomeBatterio;
-            std::cout<<Array[IndiceVuoto].Dati[k].Concentrazione;
+            cout<<Array[IndiceVuoto].Dati[k].NomeBatterio;
+            cout<<Array[IndiceVuoto].Dati[k].Concentrazione;
         }
         conta++;
-        std::cout<<std::endl;
+        cout<<endl;
     }
 }
