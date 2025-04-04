5fb0562a6e5247cf3602d8887b8e1b569d05acc5
Giuseppe di Pasquale <giuseppedipasquale@iisdaostalaquila.it>
2025-04-04 10:30:25 +0000

    Update Stampa.hpp

--- Funzioni/Stampa.hpp
+++ Funzioni/Stampa.hpp
@@ -1,14 +1,15 @@
 void Stampa(record Array[LunghezzaDatabase], unsigned int IndiceVuoto)
 {
-    int conta=0;
-    while(conta<=IndiceVuoto){
-        std::cout<<Array[IndiceVuoto].Latitudine<<"|"<<Array[IndiceVuoto].Longitudine<<<<"|"<<Array[IndiceVuoto].NumeroSpecie<<'\t';
-        for(int k=0; k<8; k++){
+    unsigned int conta=0;
+    while(conta<=IndiceVuoto)
+    {
+        std::cout<<Array[IndiceVuoto].Latitudine<<"|"<<Array[IndiceVuoto].Longitudine<<"|"<<Array[IndiceVuoto].NumeroSpecie<<'\t';
+        for(int k=0; k<8; k++)
+        {
             std::cout<<Array[IndiceVuoto].Dati[k].NomeBatterio;
             std::cout<<Array[IndiceVuoto].Dati[k].Concentrazione;
         }
         conta++;
         std::cout<<std::endl;
     }
-    return;
 }
