db1f45f5c52f9af189b18a383cabfff68234657b
Giuseppe di Pasquale <giuseppedipasquale@iisdaostalaquila.it>
2025-04-04 12:50:30 +0000

    Rimozione std::

--- Funzioni/Inserimento.hpp
+++ Funzioni/Inserimento.hpp
@@ -3,58 +3,58 @@ void Inserimento(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto)
     int Scelta;
 
     //scelta tra inserimento massivo o singolo
-    std::cout<<"Che tipo di inserimento vuoi fare?"<<std::endl;
-    std::cout<<"1)Singolo"<<std::endl;
-    std::cout<<"2)Massivo"<<std::endl;
-    std::cin>>Scelta;
+    cout<<"Che tipo di inserimento vuoi fare?"<<endl;
+    cout<<"1)Singolo"<<endl;
+    cout<<"2)Massivo"<<endl;
+    cin>>Scelta;
     bool ris=false;
 
     switch(Scelta){
 
         case 1: PulisciSchermo;
-                std::cout<<"Hai scelto l'inserimento singolo"<<std::endl;
-                std::cout<<std::endl;
-                std::cout<<"Qual è il nome del luogo?"<<std::endl;
+                cout<<"Hai scelto l'inserimento singolo"<<endl;
+                cout<<endl;
+                cout<<"Qual è il nome del luogo?"<<endl;
                 NomeLuogo(Array, IndiceVuoto);
-                std::cout<<std::endl;
-                std::cout<<"Inserisci il nome del batterio: "<<std::endl;
-                std::cin>>Array[IndiceVuoto].Dati[Array[IndiceVuoto].NumeroSpecie].NomeBatterio;
+                cout<<endl;
+                cout<<"Inserisci il nome del batterio: "<<endl;
+                cin>>Array[IndiceVuoto].Dati[Array[IndiceVuoto].NumeroSpecie].NomeBatterio;
                 for(int k=0; k<SpeciePerRecord; k++){
                     while(ris==false){
-                        std::cout<<"Inserisci la concentrazione"<<std::endl;
-                        std::cin>>Array[IndiceVuoto].Dati[k].Concentrazione;
+                        cout<<"Inserisci la concentrazione"<<endl;
+                        cin>>Array[IndiceVuoto].Dati[k].Concentrazione;
                         ris=InputDouble(Array[IndiceVuoto].Dati[k].Concentrazione)
                         if(ris==false){
-                            std::cout<<"Inserimento non valido."<<std::endl;
+                            cout<<"Inserimento non valido."<<endl;
                         }
                     }
                 }
         break;
         case 2: PulisciSchermo;
                 int NumMassivi;
-                std::cout<<"Hai scelto l'inserimento massivo"<<std::endl;
-                std::cout<<std::endl;
+                cout<<"Hai scelto l'inserimento massivo"<<endl;
+                cout<<endl;
 
-                std::cout<<"Quanti dati vuoi inserire? ";
-                std::cin>>NumMassivi;
+                cout<<"Quanti dati vuoi inserire? ";
+                cin>>NumMassivi;
                 for(int i=0; i<NumMassivi; i++)
                 {
-                    std::cout<<"Qual è il nome del "<<i+1"° luogo?"<<std::endl;
+                    cout<<"Qual è il nome del "<<i+1"° luogo?"<<endl;
                     NomeLuogo(Array, IndiceVuoto);
                     for(int j=0; j<SpeciePerRecord; j++){
-                        std::cout<<"Inserisci il nome del "<<j+1<<"° batterio: "<<std::endl;
-                        std::cin>>Array[IndiceVuoto].Dati[Array[IndiceVuoto].NumeroSpecie].NomeBatterio;
-                        std::cout<<"Inserisci la concentrazione"<<std::endl;
-                        std::cin>>Array[IndiceVuoto].Dati[Array[IndiceVuoto].NumeroSpecie].Concentrazione;
+                        cout<<"Inserisci il nome del "<<j+1<<"° batterio: "<<endl;
+                        cin>>Array[IndiceVuoto].Dati[Array[IndiceVuoto].NumeroSpecie].NomeBatterio;
+                        cout<<"Inserisci la concentrazione"<<endl;
+                        cin>>Array[IndiceVuoto].Dati[Array[IndiceVuoto].NumeroSpecie].Concentrazione;
                         ris=InputDouble(Array[IndiceVuoto].Dati[Array[IndiceVuoto].NumeroSpecie].Concentrazione)
                         if(ris==false){
-                            std::cout<<"Inserimento non valido."<<std::endl;
+                            cout<<"Inserimento non valido."<<endl;
                         }
 
                     }
                 }
         break;
-        default: std::cout<<"Opzione non valida."<<std::endl;
+        default: cout<<"Opzione non valida."<<endl;
         break;
     }
     IndiceVuoto++;
