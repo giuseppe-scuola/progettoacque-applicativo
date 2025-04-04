460c256a8cdfd72a63e2cd59b5c656c13f8df3e0
Giuseppe di Pasquale <giuseppedipasquale@iisdaostalaquila.it>
2025-04-04 19:20:06 +0000

    Aggiunta della funzionalità di eliminazione

--- Funzioni/Cancella.hpp
+++ Funzioni/Cancella.hpp
@@ -2,17 +2,42 @@ void CancellaPrompt(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto)
 {
     //Dichiarazione variabili
     bool InputValido = false;
+    unsigned int Scelta;
 
     //Loop per l'input
     while (!InputValido)
     {
         //Output per l'utente
         Stampa(Array, IndiceVuoto);
+        std::cout << "Inserisci il record da cancellare: ";
+        InputValido = InputInt(Scelta);
+
+        //Gestione input dell'utente
+        if (!InputValido)
+        {
+            //Pulizia e output per l'utente
+            PulisciSchermo;
+            std::cout << "Il valore inserito non è valido!" << std::endl;
+        }
     }
+
+    //Cancellazione dell'elemento dall'array
+    Cancella(Array, IndiceVuoto, Scelta);
+
+    //Pulizia schermo e output per l'utente
+    PulisciSchermo;
+    std::cout << "Il record è stato cancellato." << std::endl;
 }
 
 void Cancella(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto, unsigned int DaRimuovere)
 {
     //Loop per la rimozione
-    while ()
+    while (DaRimuovere < IndiceVuoto - 1)
+    {
+        Array[DaRimuovere] = Array[DaRimuovere + 1];
+        DaRimuovere++;
+    }
+
+    //Diminuzione contatore dell'array
+    IndiceVuoto--;
 }
