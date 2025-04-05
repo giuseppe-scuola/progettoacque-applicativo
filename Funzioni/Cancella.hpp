1ff355576a03088ecd851d966a2f870ea2f87caa
Giuseppe di Pasquale <giuseppedipasquale@iisdaostalaquila.it>
2025-04-05 07:59:58 +0000

    Rimozione std::

--- Funzioni/Cancella.hpp
+++ Funzioni/Cancella.hpp
@@ -9,7 +9,7 @@ void CancellaPrompt(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto)
     {
         //Output per l'utente
         Stampa(Array, IndiceVuoto);
-        std::cout << "Inserisci il record da cancellare: ";
+        cout << "Inserisci il record da cancellare: ";
         InputValido = InputInt(Scelta);
 
         //Gestione input dell'utente
@@ -17,7 +17,7 @@ void CancellaPrompt(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto)
         {
             //Pulizia e output per l'utente
             PulisciSchermo;
-            std::cout << "Il valore inserito non è valido!" << std::endl;
+            cout << "Il valore inserito non è valido!" << endl;
         }
     }
 
@@ -26,7 +26,7 @@ void CancellaPrompt(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto)
 
     //Pulizia schermo e output per l'utente
     PulisciSchermo;
-    std::cout << "Il record è stato cancellato." << std::endl;
+    cout << "Il record è stato cancellato." << endl;
 }
 
 void Cancella(record Array[LunghezzaDatabase], unsigned int &IndiceVuoto, unsigned int DaRimuovere)
