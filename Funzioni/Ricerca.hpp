180c9c3c72293784e7ff0d98b8b71fc62c7ac648
Giuseppe di Pasquale <giuseppedipasquale@iisdaostalaquila.it>
2025-04-19 19:31:36 +0200

    SI COMPILA

--- Funzioni/Ricerca.hpp
+++ Funzioni/Ricerca.hpp
@@ -1,31 +1,35 @@
 void RicercaPrompt(record Array[LunghezzaDatabase], unsigned int IndiceVuoto)
 {
-        int Scelta;
+        unsigned int Scelta = 0;
+                string Luogo;
+                string Batt;
+                        int Val;
+                int SceltaConcentrazione;
+                        int Val1, Val2;
+                string Tipo;
+
     wcout<<"Inserisci che tipo di ricerca vuoi fare?"<<endl;
     wcout<<"1)Ricerca per Luogo"<<endl;
     wcout<<"2)Ricerca per Batterio"<<endl;
     wcout<<"3)Ricerca per Concentrazione"<<endl;
     wcout<<"4)Ricerca per Tipologia"<<endl;
-    cin>>Scelta;
+    InputInt(Scelta);
     switch (Scelta)
     {
         case 1: system("cls");
                 wcout<<"Hai selezionato la ricerca per luogo."<<endl;
-                string Luogo;
                 wcout<<"Inserisci il luogo che vuoi ricercare: ";
                 getline(cin, Luogo);
                 RicercaLuogo(Array, IndiceVuoto, Luogo);
         break;
         case 2: system("cls");
                 wcout<<"Hai selezionato la ricerca per batterio."<<endl;
-                string Batt;
                 wcout<<"Inserisci il Batterio che vuoi ricercare: ";
                 getline(cin, Batt);
-                RicercaBatterio(Array, IndiceVuoto, Batt);
+                //RicercaBatterio(Array, IndiceVuoto, Batt);
         break;
         case 3: system("cls");
                 wcout<<"Hai selezionato la ricerca per concentrazione."<<endl;
-                int SceltaConcentrazione;
                 wcout<<"Come vuoi basare la ricerca?"<<endl;
                 wcout<<"1)Concentrazione minore del valore inserito"<<endl;
                 wcout<<"2)Concentrazione maggiore del valore inserito"<<endl;
@@ -35,26 +39,23 @@ void RicercaPrompt(record Array[LunghezzaDatabase], unsigned int IndiceVuoto)
                 {
                 case 1: system("cls");
                         wcout<<"Hai selezionato la ricerca per concentrazione minore di un valore inserito"<<endl;
-                        int Val;
                         wcout<<"Inserisci il valore: ";
                         cin>>Val;
-                        ConcentrazioneMinore(Array, IndiceVuoto, Val);
+                        //ConcentrazioneMinore(Array, IndiceVuoto, Val);
                 break;
                 case 2: system("cls");
                         wcout<<"Hai selezionato la ricerca per concentrazione maggiore di un valore inserito"<<endl;
-                        int Val;
                         wcout<<"Inserisci il valore: ";
                         cin>>Val;
-                        ConcentrazioneMaggiore(Array, IndiceVuoto, Val);
+                        //ConcentrazioneMaggiore(Array, IndiceVuoto, Val);
                 break;
                 case 3: system("cls");
                         wcout<<"Hai selezionato la ricerca per concentrazione compresa tra due valori"<<endl;
-                        int Val1, Val2;
                         wcout<<"Inserisci il valore 1: ";
                         cin>>Val1;
                         wcout<<"Inserisci il valore 2: ";
                         cin>>Val2;
-                        ConcentrazioneCompresa(Array, IndiceVuoto, Val1, Val2);
+                        //ConcentrazioneCompresa(Array, IndiceVuoto, Val1, Val2);
                 break;
                 default: wcout<<"Inserimento non valido."<<endl;
                 break;
@@ -62,10 +63,9 @@ void RicercaPrompt(record Array[LunghezzaDatabase], unsigned int IndiceVuoto)
         break;
         case 4: system("cls");
                 wcout<<"Hai selezionato la ricerca per tipologia di batterio."<<endl;
-                string Tipo;
                 wcout<<"Inserisci il la Tipologia che vuoi ricercare: ";
                 getline(cin, Tipo);
-                RicercaTipo(Array, IndiceVuoto, Tipo);
+                //RicercaTipo(Array, IndiceVuoto, Tipo);
         break;
         default: wcout<<"Inserimento non valido."<<endl;
         break;
