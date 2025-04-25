6c404e6806baec7706aedb690df9ef41112a0bdc
Giuseppe di Pasquale <giuseppedipasquale@iisdaostalaquila.it>
2025-04-25 20:03:05 +0200

    Aggiornamento della stampa per le nuove style guidelines

--- Funzioni/Stampa.hpp
+++ Funzioni/Stampa.hpp
@@ -13,7 +13,10 @@ void StampaArray(record Array[LunghezzaDatabase], unsigned int IndiceVuoto)
         //Stampa dei dati
         while (ContatoreDati < Array[ContatoreArray].NumeroSpecie)
         {
-            cout << "Concentrazione batterio No." << ContatoreDati + 1 << " (" << Array[ContatoreArray].Dati[ContatoreDati].NomeBatterio << "): " << Array[ContatoreArray].Dati[ContatoreDati].Concentrazione << endl;
+            cout << "<<BATTERIO No." << ContatoreDati + 1 << ">>" << endl;
+            cout << "Nome: " << Array[ContatoreArray].Dati[ContatoreDati].NomeBatterio << endl;
+            cout << "Tipologia: " << Array[ContatoreArray].Dati[ContatoreDati].Tipologia << endl;
+            cout << "Concentrazione: " << Array[ContatoreArray].Dati[ContatoreDati].Concentrazione << endl;
             ContatoreDati++;
         }
         ContatoreDati = 0;
@@ -22,19 +25,22 @@ void StampaArray(record Array[LunghezzaDatabase], unsigned int IndiceVuoto)
     }
 }
 
-void StampaRecord(record Array[LunghezzaDatabase], unsigned int IndiceDaStampare)
+void StampaRecord(record Array[LunghezzaDatabase], unsigned int RecordDaStampare)
 {
     //Dichiarazione variabili
     unsigned int Contatore = 0;
 
     //Stampa dei metadati
-    cout << "[RECORD NUMERO " << IndiceDaStampare + 1 << "]" << endl;
-    cout << "Luogo di raccolta: " << Array[IndiceDaStampare].Luogo << " (" << Array[IndiceDaStampare].Longitudine << ", " << Array[IndiceDaStampare].Latitudine << ");" << endl;
+    cout << "[RECORD NUMERO " << RecordDaStampare + 1 << "]" << endl;
+    cout << "Luogo di raccolta: " << Array[RecordDaStampare].Luogo << " (" << Array[RecordDaStampare].Longitudine << ", " << Array[RecordDaStampare].Latitudine << ");" << endl;
         
     //Stampa dei dati
-    while (Contatore < Array[IndiceDaStampare].NumeroSpecie)
+    while (Contatore < Array[RecordDaStampare].NumeroSpecie)
     {
-        cout << "Concentrazione batterio No." << Contatore + 1 << " (" << Array[IndiceDaStampare].Dati[Contatore].NomeBatterio << "): " << Array[IndiceDaStampare].Dati[Contatore].Concentrazione << endl;
+        cout << "<<BATTERIO No." << Contatore + 1 << ">>" << endl;
+        cout << "Nome: " << Array[RecordDaStampare].Dati[Contatore].NomeBatterio << endl;
+        cout << "Tipologia: " << Array[RecordDaStampare].Dati[Contatore].Tipologia << endl;
+        cout << "Concentrazione: " << Array[RecordDaStampare].Dati[Contatore].Concentrazione << endl;
         Contatore++;
     }
     cout << endl;
